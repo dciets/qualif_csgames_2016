dbcfd26f976ad08d3214a666058c82648e02e6aa
Mathieu Binette <mbinette91@users.noreply.github.com>
2016-01-29 14:44:00 -0800

    Create run.bat
    Ajout d'un exemple pour exécuter une solution sous Windows. :troll:

---
+++ run.bat
@@ -0,0 +1,9 @@
+# Ce script est un exemple pour exécuter votre solution sous Windows
+python3 solution.py
+
+# Décommentez les lignes suivantes si vous utilisez Java:
+# javac solution.java &> /dev/null
+# java solution
+
+# Ajoutez vos propres commandes si vous utilisez un autre langage.
+# Assurez-vous d'ajouter "&> /dev/null" aux commandes de compilation.
