c4e59f019746568ee90462b7bbf637cdc272d791
isra17 <isra017@gmail.com>
2016-01-30 14:02:10 -0500

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,9 +1,11 @@
+:: Cmd: runner "cmd.exe /c run.bat"
 :: Ce script est un exemple pour exécuter votre solution sous Windows
-py.exe solution.py
+:: py.exe solution.py
 
 :: Décommentez les lignes suivantes si vous utilisez Java:
-:: javac solution.java > NUL
-:: java solution
+@echo off
+javac solution.java > NUL
+java solution
 
 :: Ajoutez vos propres commandes si vous utilisez un autre langage.
 :: Assurez-vous d'ajouter "> NUL" aux commandes de compilation.
